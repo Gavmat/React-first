6fe627967651fa584a7570c7bc0297945cf293a6
Gavmat <gavrmat@yandex.ru>
2022-08-12 11:45:02 +0300

    added new file

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+
+import './App.css';
+
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+
+}
+export default App;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+
+            <div>
+                <img className='content__img' src='https://placepic.ru/wp-content/uploads/2021/02/kinopoisk_ru_Brad_Pi-41.jpg'></img>
+            </div>
+            <div>
+                <img className='content__img' src='https://catherineasquithgallery.com/uploads/posts/2021-03/1614636758_18-p-fon-gori-dlya-fotoshopa-24.jpg'></img>
+            </div>
+
+            <div>
+                Ava+Description
+            </div>
+            <div>
+                My post
+                <div> New posts
+
+                </div>
+
+                <div>
+                    <div>post1</div>
+                    <div>posts</div>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+
+export default Profile;
